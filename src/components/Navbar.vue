eae09978ab223fab09a895032bebbd8ad8c85bac
karen371 <karengodoy325@gmail.com>
2024-12-10 20:25:45 -0300

    Actualizacion de Navbar: Boton hamburgesa para responsividad

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,39 @@
 <script setup>
-import imglogo from '../assets/img/logo.png'
+import { ref } from 'vue';
+import imglogo from '../assets/img/logo.png';
 
+const menuOpen = ref(false);
+
+function toggleMenu() {
+  menuOpen.value = !menuOpen.value;
+}
 </script>
 <template>
   <nav class="fixed w-full z-20 top-0 start-0 border-b border-gray-200 nav">
     <div
       class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
     >
+      <!-- Logo -->
       <span class="flex items-center space-x-3 rtl:space-x-reverse">
         <img :src="imglogo" class="h-20" alt="sol chic logo" />
       </span>
+      
+      <!-- Botón de responsividad -->
+      <button
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+        aria-controls="navbar-sticky"
+        aria-expanded="false"
+        @click="toggleMenu"
+      >
+        <span class="sr-only">Abrir menú</span>
+        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </button>
+
+      <!-- Menú -->
       <div
-        class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+        :class="['items-center justify-between w-full md:flex md:w-auto md:order-1', { hidden: !menuOpen }]"
         id="navbar-sticky"
       >
         <ul
